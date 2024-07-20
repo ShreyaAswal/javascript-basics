14653db965b8ed90cad204348d8b4fac71876b8c
ShreyaAswal <aswalshreya.227@gmail.com>
2024-07-20 09:54:56 +0000

    datatypes

---
+++ js_basics/datatypes.js
@@ -0,0 +1,17 @@
+"use strict"; //treat all js code as newer version
+//alert(3+3) //int node dont do this function with alert syntax
+ 
+//number -> 2 to the power 53
+//bigint for very large numbers
+//string=""
+//null ->standalone value
+//undefined -> // initial value ~ empty value ~ value not defined
+//symbol -> uniqueness (used to identify that one component is uniwue from other component)
+
+
+//object
+console.log(typeof "shreya")
+let age=33
+console.log(typeof age) 
+console.log(typeof null) // type of null is ~ object
+console.log(typeof undefined) //typeof undefined is ~ undefined(it is a type itself)
