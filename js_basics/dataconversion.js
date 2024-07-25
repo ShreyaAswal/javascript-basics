394d067008d6f83a594b0af790a41ec8d7ef7237
ShreyaAswal <aswalshreya.227@gmail.com>
2024-07-25 17:20:38 +0000

    datatype conversion

---
+++ js_basics/dataconversion.js
@@ -0,0 +1,10 @@
+let score=33
+console.log(typeof score) //number 
+let s="33abc"
+let y=Number(s);
+console.log(typeof y) //number
+console.log(y)        //NAN
+let n=null
+n=String(n);
+console.log(typeof n) //string
+console.log(n) //null
