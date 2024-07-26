7ce3b053809f8b8c1a1aee7c9b620bd750862456
ShreyaAswal <aswalshreya.227@gmail.com>
2024-07-26 14:13:08 +0000

    stack and heap memory

--- js_basics/datatypesSummary.js
+++ js_basics/datatypesSummary.js
@@ -22,3 +22,17 @@ let myObj={
 
 let myArr=["abc","def","ghi"]
 console.log(typeof myArr) //object
+
+//*******************Memory******************/
+//stack(primitive types) and heap(non primitive types)
+let x=2
+let y=x
+y=5
+console.table([x,y]) // x=2 and y=5
+let obj1={
+    name:"shreya",
+    age:22
+}
+let obj2=obj1
+obj2.name="Tanu"
+console.table([obj1.name,obj2.name]) //both Tanu
