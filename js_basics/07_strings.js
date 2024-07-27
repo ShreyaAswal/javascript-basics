5c9dc8f26218120e9e727943dd24a8c5dd391873
ShreyaAswal <aswalshreya.227@gmail.com>
2024-07-27 16:22:36 +0000

    strings

---
+++ js_basics/07_strings.js
@@ -0,0 +1,9 @@
+let str= new String("shreya")
+//console.log(str.slice(-6,-4))
+//console.log(str.slice(0,4))
+let s=new String("     tanu    ")
+//console.log(s.trim())
+const url="https//:google.shreya%20com/"
+//console.log(url.replace('com','cs'))
+//console.log(url.includes('shreya'))
+console.log(url.split('/'))
