9dd4e01163a2feccaf4020b5d883b62fed8ffac8
ShreyaAswal <aswalshreya.227@gmail.com>
2024-07-28 07:36:26 +0000

    nums and maths

---
+++ js_basics/08_nums_and_maths.js
@@ -0,0 +1,8 @@
+let n= new Number(346.64569162);
+//console.log(n)
+//console.log(n.__proto__)
+//console.log(n.toPrecision(2))
+//console.log(Math)
+let max=28
+let min=15
+//console.log(Math.floor(Math.random()*(max-min))+min) //[15,28)
