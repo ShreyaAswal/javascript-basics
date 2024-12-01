9d247a646a80cce4e52373ae38da2859b75af642
ShreyaAswal <aswalshreya.227@gmail.com>
2024-12-01 13:50:38 +0000

    object

--- 02_basics/03_objects.js
+++ 02_basics/03_objects.js
@@ -23,5 +23,25 @@ const obj2={
     age:22
 }; 
 //console.log(obj2[sym]);
-console.log(typeof obj2[sym]);
+//console.log(typeof obj2[sym]);
+
+//obj2.name="shreya aswal"//for overwriting/creating a key in object
+//Object.freeze(obj2) //Prevents modification of existing property attributes and values, and prevents the addition of new properties(freezes the object in its current state).
+//obj2.name="shr"
+//console.log(obj2)
+
+//in js we treat the function same as we treat a variable
+
+obj2.greeting= function(){
+    console.log("hello")
+}
+//console.log(obj2.greeting) //function didnt execute , we get the reference of the function
+//console.log(obj2.greeting()); //func did get execute
+
+obj2.greet=function(){
+    console.log(`hello ${this.name}`); //if we want to use a data of the class only we can use #this keyword as well, as this stores the refernce of the object
+}
+console.log(obj2.greet());
+
+
 
