5dc0324808c7a3bc5e975725aed340bd55538c05
ShreyaAswal <aswalshreya.227@gmail.com>
2024-12-01 11:53:02 +0000

    array

---
+++ 02_basics/02_arrays.js
@@ -0,0 +1,24 @@
+const arr1=[1,2,3,4,5];
+const arr2=["marvel", "dc", "disney", "hbo"];
+const arr3=["deshaw", "visa", "kla", "adobe"];
+//arr1.push(arr2) // it pushes the arr2 as an element in arr1
+const arr4=arr1.concat(arr2,arr3); //Combines two or more arrays. This method returns a new array without modifying any existing arrays.
+//console.log(arr1,arr4);
+
+//diff b/w spread operator and concat function is that, spread iterate over the string/array and then put it into the array
+
+const arr5=[...arr1,...arr2]; //spread operator
+//console.log(arr5);
+
+const arr6=[1,2,3,[4,5,6],[7,8,[9,10,[11,12]]],13,14];
+
+const arr7=arr6.flat(Infinity) //Returns a new array with flattening its depth upto given number (give infinity, if want to flat whole arr)
+//console.log(arr7);
+
+//console.log(Array.isArray("shreya")); //return whether the arg is arr or not
+//console.log(Array.from("shreya")); //Creates an array from an iterable object.
+
+let x=100;
+let y=101;
+let z=102;
+//console.log(Array.of(x,y,z));//Returns a new array from a set of elements.
