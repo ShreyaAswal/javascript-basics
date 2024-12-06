f03e2f935044fa1109a9a189d7615e4023125842
ShreyaAswal <aswalshreya.227@gmail.com>
2024-12-06 07:25:43 +0000

    while

---
+++ 05_iterators/while.js
@@ -0,0 +1,15 @@
+//while loop
+
+let i=0
+
+//entry controlled loop
+// while (i<10) {
+//     console.log(i);
+//     i++;
+// }
+
+let j=11
+//exit controlled loop (loop runs atleast once)
+do {
+    console.log(j)
+} while (j<10);
